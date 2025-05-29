a0a90c22ad68ed3f48a6347f2a1848bf4813190e
Rohith sai <rohithsaimidigudla@gmail.com>
2025-05-30 01:40:43 +0530

    Updates

--- src/tools/box_office_predictor.py
+++ src/tools/box_office_predictor.py
@@ -1,40 +1,31 @@
 # trope_detector.py
-from langchain_core.tools import tool
 from src.config import settings
-from src.utils.helpers import get_llm
-from langgraph.agents import Agent
-from src.prompts.prompts import TROPE_DETECTOR_PROMPT
 from crawl4ai import AsyncCrawler
 import requests
 from src.config.logger import logger
+from langchain_core.tools import tool
 
-class BoxOfficePredictor:
-    def __init__(self, query: str):
-        self.SERPER_API_KEY = settings.SERPER_API_KEY
-        self.base_url = "https://google.serper.dev/search"
-        self.payload = {
-            "q": "site:the-numbers.com " + query,
-            "api_key": self.SERPER_API_KEY
-        }
-        self.headers = {
-            'X-API-KEY': self.SERPER_API_KEY,
-            'Content-Type': 'application/json'
-        }
-        
-    async def predict(self):
-        logger.info(f"Predicting box office for {self.payload}")
-        response = requests.request("POST", self.base_url, headers=self.headers, data=self.payload)
-        return response.json()
+@tool
+def box_office_predictor(query: str,num_results: int = 5) -> str:
     
-    @tool
-    async def get_movie_info(self) -> str:
-        urls =  await self.predict()
-        for url in urls["organic_results"]:
-            response = await AsyncCrawler().get(url["link"])
-            return response.markdown
-        
-    tools = [
-        get_movie_info,
-    ]
-
-box_office_predictor = BoxOfficePredictor(query="Avatar")
+    api_key = settings.SERPER_API_KEY
+    base_url = "https://google.serper.dev/search"
+    payload = {
+        "q": "site:the-numbers.com " + query,
+        "api_key": api_key
+    }
+    headers = {
+        'X-API-KEY': api_key,
+        'Content-Type': 'application/json'
+    }
+    
+    response = requests.request("POST", base_url, headers=headers, data=payload)
+    urls = response.json()
+    
+    crawler = AsyncCrawler()
+    results = []
+    for url in urls["organic_results"][:num_results]:
+        response = crawler.get(url["link"])
+        results.append(response.markdown)
+    
+    return "\n".join(results)
--- src/tools/imdb_api.py
+++ src/tools/imdb_api.py
@@ -5,36 +5,27 @@ from langchain_core.tools import tool
 from crawl4ai import AsyncCrawler
 from src.config.logger import logger
 
-class IMDbAPI:
-    def __init__(self):
-        self.api_key = settings.SERPER_API_KEY
-        self.base_url = "https://google.serper.dev/search"
-        
+@tool
+def imdb_api(query: str,num_results: int = 5) -> str:
+    api_key = settings.SERPER_API_KEY
+    base_url = "https://google.serper.dev/search"
+    payload = {
+        "q": "site:imdb.com " + query,
+        "api_key": api_key
+    }
+    headers = {
+        'X-API-KEY': api_key,
+        'Content-Type': 'application/json'
+    }
+    response = requests.request("POST", base_url, headers=headers, data=payload)
+    logger.info(f"Serching the web for: {query}")
+    urls = response.json()
     
-    async def search(self, query: str) -> str:
-        logger.info(f"Searching for {query}")
-        payload = {
-            "q": "site:imdb.com " + query,
-            "api_key": self.api_key
-        }
-        headers = {
-            'X-API-KEY': self.api_key,
-            'Content-Type': 'application/json'
-        }
-        response = requests.request("POST", self.base_url, headers=headers, data=payload)
-        return response.json()
+    crawler = AsyncCrawler()
+    results = []
+    for url in urls["organic_results"][:num_results]:
+        response = crawler.get(url["link"])
+        results.append(response.markdown)
     
-    @tool
-    async def get_movie_info(self, query: str) -> str:
-        logger.info(f"Getting movie info for {query}")
-        urls =  await self.search(query)
-        crawler = AsyncCrawler()
-        for url in urls["organic_results"]:
-            response = await crawler.get(url["link"])
-            return response.markdown
-        
-    tools = [   
-        get_movie_info,
-    ]
-
-imbdapi = IMDbAPI()
+    return "\n".join(results)
+    
